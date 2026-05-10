3307b2006738a75cac8e6c6e6a9591ff11b632d0
kameldeeb <63444490+kameldeeb@users.noreply.github.com>
2026-05-10 20:03:10 +0300

    FIX: Implement values method in Condition enum
    Add a static method to retrieve all condition values.

--- app/Enums/Condition.php
+++ app/Enums/Condition.php
@@ -8,4 +8,9 @@ enum Condition: string
     case GOOD = 'good';
     case FAIR = 'fair';
     case NEEDS_REPAIR = 'needs_repair';
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
 }
